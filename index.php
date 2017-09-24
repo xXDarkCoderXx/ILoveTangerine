aea802e4e24ded8e8b54400bf8d8e3c4d5204c8e
xXDarkCoderXx <32234323+xXDarkCoderXx@users.noreply.github.com>
2017-09-23 22:12:32 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,188 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KXS63QW');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106936526-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-106936526-1');
+</script>
+
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Refer a friend program page for Tangerine new clients. Use the Orange Code on this site to receive a sign-up bonus as a new customer.">
+    <meta name="author" content="I Love Tangerine">
+    <meta name="keywords" content="tangerine, tangerine coupon, orange code, orange key, referral code, coupon code, offer code, retailmenot, redflagdeals, red flag deals, canada, banking">
+
+    <title>I ❤ Tangerine | Referral Code for New Members</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-clean-blog/3.3.7+1/css/clean-blog.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KXS63QW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.php">I ❤ Tangerine</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-color: #F58426">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1>Tangerine Orange Key</h1>
+                        <hr class="small">
+                        <span class="subheading">Use this referral code to get a <b>$50</b> bonus when you sign up for a new Tangerine account:</span>
+                        <center><p style="
+                            font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                            color: white;
+                            padding: 5px;
+                            background-color: #003C78;
+                            margin-top: 50px;
+                            width: 200px;
+                        "><b>41947218S1</b></p><center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="post-preview">
+                        <h2 class="post-title">
+                            How to get $50 for joining Tangerine
+                        </h2>
+
+                        <p>Hi there!</p>
+
+                        <p>I’m OuterRaven, from Canada. I’m a Tangerine fan.</p>
+
+                        <p>I like their well thought out interface, and not paying banking fees like the other banks.</p>
+
+                        <p>Please find my referral code below to get $50 when you open any savings or chequing account.</p>
+
+                        <p>Simply copy and paste the code below into the “Orange Key” field when you sign up on the site.</p>
+
+                        <p>Happy banking. ;-)</p>
+
+                        <p>OuterRaven</p>
+
+                        <p>P.S. Update: The $50 deal is up until October 31, 2017!</p>
+
+                        <br>
+                        <div style="background-color: #E7E7E7; padding:15px">
+                        <center><h4 style="margin:5px;">Tangerine Orange Key:</h4><center>
+                        <center><p style="
+                            font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                            color: white;
+                            padding: 5px;
+                            background-color: #003C78;
+                            width: 200px;
+                            display:inline-block;
+                            margin:5px;
+                        "><b>41947218S1</b></p><center></div>
+                        <br>
+
+                        <p> </p>
+                        <h2 class="post-title">
+                            What's next?
+                        </h2>
+                        <p>It's simple!</p>
+                        <p>1. Go to <a href="https://www.tangerine.ca/en/sign-me-up/index.html">tangerine.com</a> and select the savings or chequing account that best fits your needs.</p>
+                        <p>2. Copy and paste the Tangerine Orange Key (41947218S1) into your application form when prompted for your $50 bonus!</p>
+                        
+
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <p class="copyright text-muted">Copyright &copy; ILoveTangerine 🇨🇦 2017</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
